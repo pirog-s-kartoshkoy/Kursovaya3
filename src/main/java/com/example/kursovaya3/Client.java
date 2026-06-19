85e923231017083634b4ad4c8681693455b2d570
Николай <kollymatr@gmail.com>
2026-06-20 00:33:26 +0300

    Теперь добавлена 3 вкладка, а так же написана функция, которая эту вкладку оформляет. Осталось теперь добавить возможность редактирования базы данных и все будет класс

--- src/main/java/com/example/kursovaya3/Client.java
+++ src/main/java/com/example/kursovaya3/Client.java
@@ -3,15 +3,18 @@ package com.example.kursovaya3;
 public class Client {
     private int idClient;
     private String fullName;
+    private String gender;
     private String phone;
 
-    public Client(int idClient, String fullName, String phone) {
+    public Client(int idClient, String fullName, String gender, String phone) {
         this.idClient = idClient;
         this.fullName = fullName;
+        this.gender = gender;
         this.phone = phone;
     }
 
     public int getIdClient() { return idClient; }
     public String getFullName() { return fullName; }
+    public String getGender() { return gender; }
     public String getPhone() { return phone; }
 }
